0a9c9fc7667162bfd9de398a6459059de5da07d4
MillaAlex <121360467+MillaAlex@users.noreply.github.com>
2023-04-15 11:48:40 +0300

    2k up to N - added 2nd option

--- Task14.py
+++ Task14.py
@@ -2,6 +2,7 @@
 # вида 2k), не превосходящие числа N.
 # 10 -> 1 2 4 8
 
+#option 1:
 n = int(input("Введите число N: "))
 k = 1
 i = 1
@@ -9,3 +10,10 @@ print(n, end=' ' "-> ")
 while (i * k) <= n:
     print(i * k, end=' ')
     k *= 2
+    
+#option 2:
+n = int(input("Введите число N: "))
+i = 0
+while 2 ** i <= n:
+    print(2 ** i)
+    i += 1
