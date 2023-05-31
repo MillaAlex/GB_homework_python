263cd59c381d033cef368b0d1c85fc6402bd0b26
Lyudmila <liudmila.k1610@gmail.com>
2023-05-31 23:46:19 +0300

    one-hot encoding, dummies

---
+++ Task44_dataframe.py
@@ -0,0 +1,41 @@
+# Задача 44: В ячейке ниже представлен код генерирующий DataFrame, которая состоит всего
+# из 1 столбца. Ваша задача перевести его в one hot вид. Сможете ли вы это сделать без
+# get_dummies?
+
+import numpy as np
+import pandas as pd
+import random
+
+lst = ['robot'] * 10
+lst += ['human'] * 10
+random.shuffle(lst)
+data = pd.DataFrame({'whoAmI':lst})
+print(data.head())
+print(data.shape)
+
+
+# NaN / 1.0 / 0.0:
+data.loc[data['whoAmI'] == 'human', 'human'] = 1
+data.loc[data['whoAmI'] == 'robot', 'robot'] = 0
+print(data)
+
+
+# 0 & 1 in one column:
+data = pd.DataFrame({'whoAmI':lst})
+data['whoAmI'] = np.where(data['whoAmI'].str.contains('human'), 1, 0)
+print(data)
+
+
+# get_dummies True & False:
+data = pd.DataFrame({'whoAmI':lst})
+print(pd.get_dummies(data, columns=['whoAmI']))
+
+
+# get_dummies 0 & 1 :
+data = pd.DataFrame({'whoAmI':lst})
+res = (data.drop(columns='whoAmI').join(data['whoAmI'].str.get_dummies(sep=', ')))
+print(res)
+
+
+# get_dummies Syntax:
+pd.get_dummies(data, prefix=None, prefix_sep='_', dummy_na=False, columns=None, sparse=False, drop_first=False, dtype=None)
